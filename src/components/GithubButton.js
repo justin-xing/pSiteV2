044cd6c7df8168713d278895c636e2fb1544331a
Justin Xing <justinxing3@gmail.com>
2023-05-30 09:41:41 -0400

    adjust linking in header buttons

--- src/components/GithubButton.js
+++ src/components/GithubButton.js
@@ -6,7 +6,7 @@ import Typography from '@mui/material/Typography';
 const GithubButton = () => {
   return (
   <div style={{display: 'flex', alignItems: 'center'}}>
-    <IconButton href='https://github.com/justin-xing' size='large'>
+    <IconButton href='https://github.com/justin-xing' target='_blank' size='large'>
       <GitHubIcon fontSize='inherit'/>
       <Typography variant='h6' marginLeft='5px'>Github</Typography>
     </IconButton>
--- src/components/ResumeButton.js
+++ src/components/ResumeButton.js
@@ -2,12 +2,13 @@ import React from "react";
 import ArticleIcon from '@mui/icons-material/Article';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import resume from '../assets/Resume.pdf'
 
 
 const ResumeButton = () => {
   return (
     <div style={{ display: "flex", alignItems: "center" }}>
-      <IconButton href="https://github.com/justin-xing" size="large">
+      <IconButton href={resume} target='_blank' size="large">
         <ArticleIcon fontSize="inherit" />
         <Typography variant="h6" marginLeft="5px">
           Resume
